3fa5a6bb33f0e090671ec7f7b57efad92c737e32
netas369 <neve0023@hz.nl>
2024-06-04 19:42:42 +0200

    created routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Models\Enquiry;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +12,31 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
+--------------------------------
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+---------------------------
+use App\Http\Controllers\UserController;
+
+Route::get('/user', [UserController::class, 'index']);
 */
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function() {
+    //
+});
+
+Route::post('/enquiry', function(Request $request) {
+
+});
+
+Route::delete('/enquiry/{enquiry}', function(Enquiry $enquiry){
+
+});
+
